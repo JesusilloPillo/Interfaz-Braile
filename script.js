4082d5e6c103758dca6a429d304903f5b7faaa55
JesusilloPillo <89419770+JesusilloPillo@users.noreply.github.com>
2021-11-22 18:40:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const Mensaje = document.getElementById('mensaje');
+
+function Sed() {
+    Mensaje.textContent = 'Tengo Sed';
+}
+
+function Bañar() {
+    Mensaje.textContent = 'Quiero tomar una ducha';
+}
+
+function Baño() {
+    Mensaje.textContent = 'Tengo ganas de ir al Baño';
+}
+
+function Fotos() {
+    Mensaje.textContent = 'Quiero ver Fotos de mis Familiares';
+}
+
+function Dormir() {
+    Mensaje.textContent = 'Tengo sueño';
+}
+
+function Tele() {
+    Mensaje.textContent = 'Quiero ver Netflix';
+}
+
+function Frio() {
+    Mensaje.textContent = 'Tengo Frío';
+}
+
+function Calor() {
+    Mensaje.textContent = 'Tengo Calor';
+}
+
+function Musica() {
+    Mensaje.textContent = 'Quiero escuchar Musica';
+}
+
+function Compras() {
+    Mensaje.textContent = 'Quiero ir de compras';
+}
+
+function Juegos() {
+    Mensaje.textContent = 'Quiero ver videojuegos';
+}
+
+function Comer() {
+    Mensaje.textContent = 'Tengo hambre';
+}
+
+function Leer() {
+    Mensaje.textContent = 'Tengo ganas de leer';
+}
+
+function Medico() {
+    Mensaje.textContent = 'Llama a mi Medico';
+}
+
+function Medicinas() {
+    Mensaje.textContent = 'Dame mis medicinas';
+}
+
+function Aire() {
+    Mensaje.textContent = 'Quiero aire fresco';
+}
